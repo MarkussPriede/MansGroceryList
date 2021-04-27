dd4186a46dd81a4407b8f5608103581e94af8034
MarkussPriede <markusspriede@inbox.lv>
2021-04-27 13:16:35 +0300

    Ir funkcionalitate, nav izskats

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const POP_UP = document.getElementById("popUp");
+let preces = [];
+
+window.addEventListener("load", () => {
+  preces = JSON.parse(localStorage.getItem("preces") || "[]");
+  console.log(preces);
+  render();
+});
+
+document.getElementById("jaunaPrece").addEventListener("click", () => {
+  POP_UP.style.display = "block";
+});
+
+document.getElementById("pievienotPreci").addEventListener("click", () => {
+  POP_UP.style.display = "none";
+
+  let prece = { nosaukums: nosaukums.value, daudzums: daudzums.value };
+
+  nosaukums.value = "";
+  daudzums.value = "";
+
+  preces.push(prece);
+
+  render();
+});
+
+function render() {
+  let saraksts = document.getElementById("saraksts");
+  saraksts.innerHTML = "";
+
+  for (let i = 0; i < preces.length; i++) {
+    let prece = `
+        <div class="prece">
+            <h2 class="sadala">Preces nosaukums: ${preces[i].nosaukums}</h2>
+            <h2>Daudzums: ${preces[i].daudzums}</h2>
+        </div>`;
+
+    saraksts.innerHTML += prece;
+  }
+
+  localStorage.setItem("preces", JSON.stringify(preces));
+}
